16ac6bbe388634d82697eed9ecdd282029ff2634
viper23 <htonnycarlos@gmail.com>
2022-01-21 21:59:00 +0200

    simple changes

--- recordstore/settings.py
+++ recordstore/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+INTERNAL_IPS = ['127.0.0.1']
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
