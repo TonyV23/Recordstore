8bebeb01b74bb4865d21beb096cf92195c2c40cb
viper23 <htonnycarlos@gmail.com>
2022-01-22 22:31:49 +0200

    view for lsiiting albums

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from email import message
+from django.http import HttpResponse
 
-# Create your views here.
+from store.models import ALBUMS
+
+def listing_album_view(request):
+    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
+    message = """<ul>{}</ul>""".format("\n".join(albums))
+    return HttpResponse(message)
