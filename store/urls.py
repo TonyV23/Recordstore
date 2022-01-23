acee1a9730699648ce6dff0985760c7f6b96dbdb
viper23 <htonnycarlos@gmail.com>
2022-01-23 23:03:13 +0200

    added detail_album view

--- store/urls.py
+++ store/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listing_album_view),
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail_album_view)
 ]
--- store/views.py
+++ store/views.py
@@ -8,3 +8,10 @@ def listing_album_view(request):
     albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
     message = """<ul>{}</ul>""".format("\n".join(albums))
     return HttpResponse(message)
+
+def detail_album_view(request, album_id):
+    id = int(album_id)
+    album = ALBUMS [id]
+    artists = " ".join([artist['name'] for artist in album ['artists']])
+    message = " Le nom de l'album est {} . il a été ecrit par {} ".format(album['name'], artists)
+    return HttpResponse (message)
