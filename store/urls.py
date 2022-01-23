8469c2c8f06ebd0238f199b5ab654bb6594adfb4
viper23 <htonnycarlos@gmail.com>
2022-01-23 23:33:42 +0200

    added search album view

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.listing_album_view),
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail_album_view)
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail_album_view),
+    url(r'^search/$', views.search_album_view),
 ]
--- store/views.py
+++ store/views.py
@@ -15,3 +15,23 @@ def detail_album_view(request, album_id):
     artists = " ".join([artist['name'] for artist in album ['artists']])
     message = " Le nom de l'album est {} . il a été ecrit par {} ".format(album['name'], artists)
     return HttpResponse (message)
+
+def search_album_view(request):
+    query = request.GET.get('query')
+    if not query :
+        message = " Aucun artiste n'est demandé "
+    else :
+        albums = [
+            album for album in ALBUMS
+            if query in " ".join(artist['name'] for artist in album['artists'])
+        ]
+
+        if len(albums) == 0 :
+            message = "Misère de misère, aucun resultat !"
+        else :
+            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            message = """
+                Les resultats de votre recherche :
+                    <ul>{}</ul>
+            """.format("<li></li>".join(albums))
+    return HttpResponse(message)
