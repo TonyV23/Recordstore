9f8e320b0f20c2f3da3c0ba9e850bbbe65693c6e
viper23 <htonnycarlos@gmail.com>
2022-01-24 12:50:02 +0200

    changed behave of views from simple dictionnary to db

--- store/views.py
+++ store/views.py
@@ -1,35 +1,41 @@
 # from django.shortcuts import render
-from email import message
 from django.http import HttpResponse
 
-# from store.models import ALBUMS
+from .models import Album, Artist, Booking, Contact
+
+def home_view(request):
+    albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
+    return HttpResponse(message)
 
 def listing_album_view(request):
-    albums = ["<li>{}</li>".format(album['name']) for album in ALBUMS]
-    message = """<ul>{}</ul>""".format("\n".join(albums))
+    albums = Album.objects.filter(available=True)
+    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
+    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
     return HttpResponse(message)
 
 def detail_album_view(request, album_id):
-    id = int(album_id)
-    album = ALBUMS [id]
-    artists = " ".join([artist['name'] for artist in album ['artists']])
-    message = " Le nom de l'album est {} . il a été ecrit par {} ".format(album['name'], artists)
+    album =Album.objects.get(pk =album_id)
+    artists = "".join([artist.name for artist in album.artists.all()])
+    message = " Le nom de l'album est {} . il a été ecrit par {} ".format(album.title, artists)
     return HttpResponse (message)
 
 def search_album_view(request):
     query = request.GET.get('query')
     if not query :
-        message = " Aucun artiste n'est demandé "
+        albums = Album.objects.all()
     else :
-        albums = [
-            album for album in ALBUMS
-            if query in " ".join(artist['name'] for artist in album['artists'])
-        ]
-
-        if len(albums) == 0 :
-            message = "Misère de misère, aucun resultat !"
+        albums = Album.objects.filter(title__icontains=query)
+        
+        if not albums.exists():
+            albums = Album.objects.filter(artists__name__icontains=query)
+        
+        if not albums.exists():
+            message = "Y a R Viper23"
+        
         else :
-            albums = ["<li>{}</li>".format(album['name']) for album in albums]
+            albums = ["<li>{}</li>".format(album.title) for album in albums]
             message = """
                 Les resultats de votre recherche :
                     <ul>{}</ul>
